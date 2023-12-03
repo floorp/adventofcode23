56dc9f94ebfcfc1d735209ff6a5f91f63e9273b3
floorp <aachandler89@gmail.com>
2023-12-03 11:17:05 -0500

    Update P2-dicepower.py

--- AdventofCode/2023Advent/day2/P2-dicepower.py
+++ AdventofCode/2023Advent/day2/P2-dicepower.py
@@ -1,20 +1,15 @@
 import re
-#the goal is to find which games could have equal to or less than 12R, 13G, 14B cubes per game
-#will need to find the maximum amount of dice per color is pulled per game and compare it against the goal
-#throw out the other results and add up the numerical value of the game IDs
 
 pointCheck = {"red" : 12,
               "green": 13,
               "blue" : 14}
 totalscore = 0
-passcheck = []
 redvals = []
 bluevals = []
 greenvals = []
 totalvalueGame = 0
 def start():
     global totalscore
-    global passcheck
     global totalvalueGame
     totalscore = 0
     f = open("inputtest", "r")
@@ -23,28 +18,20 @@ def start():
         getgameID = line.split(":")
         cleangameid = int(''.join(re.compile(r'\d').findall(getgameID[0])))
         
-        #passcheck[:] = []
         dicelist = getgameID[1].split(";")
         for hands in dicelist:
             hands = hands.split(",")
             for dicecount in hands:
                 dicecount = ''.join(dicecount)
                 dicecount = dicecount.split(" ")
-                #print(dicecount[1])
-                #print(dicecount[2])
                 valuecheck(dicecount, cleangameid)
-        
-        #if "fail" not in str(passcheck):
-            #print(cleangameid)
-            #print(str(passcheck))
-            #totalscore = totalscore + cleangameid
+              
     totalvalueGame = int(max(redvals))*int(max(bluevals))*int(max(greenvals))
     print(totalvalueGame)   
     #print("final result is {}".format(totalscore))
 
                         
 def valuecheck(valuecheck, gameid):
-    global passcheck
     global redvals
     global bluevals
     global greenvals
