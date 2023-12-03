878ffa586330af36e97cc466a0e950fcb7506253
floorp <aachandler89@gmail.com>
2023-12-03 13:41:58 -0500

    Update P2-dicepower.py

--- AdventofCode/2023Advent/day2/P2-dicepower.py
+++ AdventofCode/2023Advent/day2/P2-dicepower.py
@@ -1,18 +1,28 @@
 import re
+#the goal is to find which games could have equal to or less than 12R, 13G, 14B cubes per game
+#will need to find the maximum amount of dice per color is pulled per game and compare it against the goal
+#throw out the other results and add up the numerical value of the game IDs
 
 pointCheck = {"red" : 12,
               "green": 13,
               "blue" : 14}
 totalscore = 0
-redvals = []
-bluevals = []
-greenvals = []
+passcheck = []
+redvals = 0
+bluevals = 0
+greenvals = 0
 totalvalueGame = 0
+totalvalueAll = 0
 def start():
     global totalscore
+    global passcheck
     global totalvalueGame
+    global totalvalueAll
+    global redvals
+    global bluevals
+    global greenvals
     totalscore = 0
-    f = open("inputtest", "r")
+    f = open("inputraw", "r")
     for line in f.readlines():
 
         getgameID = line.split(":")
@@ -25,25 +35,32 @@ def start():
                 dicecount = ''.join(dicecount)
                 dicecount = dicecount.split(" ")
                 valuecheck(dicecount, cleangameid)
-              
-    totalvalueGame = int(max(redvals))*int(max(bluevals))*int(max(greenvals))
-    print(totalvalueGame)   
-    #print("final result is {}".format(totalscore))
+        totalvalueGame = int(redvals)*int(bluevals)*int(greenvals)
+        redvals = 0
+        bluevals = 0
+        greenvals = 0
+        print("totalvalueGame = {}".format(totalvalueGame))
+        totalvalueAll = totalvalueAll + totalvalueGame 
+    print("totalvalueall = {}".format(totalvalueAll))
+    print(totalvalueAll)   
 
                         
 def valuecheck(valuecheck, gameid):
+    global passcheck
     global redvals
     global bluevals
     global greenvals
 
     if str(valuecheck[2]) == "red" or str(valuecheck[2]) == "red\n":
-        redvals.append(valuecheck[1])
+        if int(valuecheck[1]) > redvals:
+            redvals = int(valuecheck[1])
     elif str(valuecheck[2]) == "blue" or str(valuecheck[2]) == "blue\n":
-        bluevals.append(valuecheck[1])
+        if int(valuecheck[1]) > bluevals:
+            bluevals = int(valuecheck[1])
     elif str(valuecheck[2]) == "green" or str(valuecheck[2]) == "green\n":
-        greenvals.append(valuecheck[1])
+        if int(valuecheck[1]) > greenvals:
+            greenvals = int(valuecheck[1])
 
-    #print(gameid)
 
 start()            
 
